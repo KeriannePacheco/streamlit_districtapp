1a674a83bd578c6d5f9f0ec9322c1a917c4a1ded
KeriannePacheco <48220058+KeriannePacheco@users.noreply.github.com>
2023-12-19 18:44:35 -0700

    Update Eastern Recruiting Region.py

--- pages/Eastern Recruiting Region.py
+++ pages/Eastern Recruiting Region.py
@@ -5,7 +5,7 @@ import plotly.express as px
 
 st.title("District ERR RSS Quadrant")
 local_string = r"C:\Users\kpacheco\Documents\USMC\District\District ERR RSS_Index 2023.csv"
-git_string = r"https://github.com/KeriannePacheco/streamlit_districtapp/blob/main/District%20ERR%20RSS_Index%202023.csv?raw=true"
+git_string = r"https://raw.githubusercontent.com/KeriannePacheco/streamlit_districtapp/main/District%20ERR%20RSS_Index%202023.csv"
 
 @st.cache_data 
 def import_data(path):
